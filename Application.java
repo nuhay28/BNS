02488eab9a4b254409a382ad279e982f3379b1ad
Nuhamin <nuhay2828@gmail.com>
2025-12-31 16:03:40 +0300

    update

--- Application.java
+++ Application.java
@@ -25,3 +25,17 @@ public class Application extends JFrame {
         cardLayout.show(container, "HOME"); // Start at Home
         setVisible(true);
     }
+    
+     public void showPage(String name) {
+        cardLayout.show(container, name);
+    }
+
+    public static void main(String[] args) {
+        // Use a modern Look and Feel if possible
+        try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); } 
+        catch (Exception e) {}
+        new Application();
+    }
+}
+
+
