941aaeab01e0bc96e05a180b68f7fb1c59d3f9de
Netsanet Demsie <netsanetdemsie1377@gmail.com>
2026-01-06 10:23:30 -0800

    Create LoginPanel skeleton class with imports

---
+++ BNS/src/main/views/LoginPanel.java
@@ -0,0 +1,11 @@
+package main.views;
+
+import javax.swing.*;
+import java.awt.*;
+import main.Application;
+import main.services.AuthService;
+
+public class LoginPanel extends JPanel {
+    // TODO: implement UI and logic
+}
+
