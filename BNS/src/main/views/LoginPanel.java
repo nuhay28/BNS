dbcdd79ef9315deb9b25d8e2dbdd7f87791e84b8
Netsanet Demsie <netsanetdemsie1377@gmail.com>
2026-01-06 10:35:55 -0800

    Add UI fields and AuthService instance to LoginPanel

--- BNS/src/main/views/LoginPanel.java
+++ BNS/src/main/views/LoginPanel.java
@@ -6,6 +6,8 @@ import main.Application;
 import main.services.AuthService;
 
 public class LoginPanel extends JPanel {
-    // TODO: implement UI and logic
+    private JTextField emailField;
+    private JPasswordField passField;
+    private AuthService authService = new AuthService();
 }
 
