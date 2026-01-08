e0f7c4919d55af811c397706fdead10b9cd6b43f
meklit <meklitbzuneh@gmail.com>
2026-01-08 13:02:58 +0300

    Updated AboutPanel panel

--- BNS/src/main/views/AboutPanel.java
+++ BNS/src/main/views/AboutPanel.java
@@ -4,3 +4,13 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import main.Application;
+
+public class AboutPanel extends JPanel {
+    public AboutPanel(Application app) {
+        setLayout(new BorderLayout());
+        setBackground(Color.WHITE);
+
+        // Header Section
+        JPanel header = new JPanel(new BorderLayout());
+        header.setBackground(Color.WHITE);
+        header.setBorder(new EmptyBorder(40, 40, 20, 40));
