88f0d7f963ae83bc2cb844eb0cea2bfe4c6fd2b9
Netsanet Demsie <netsanetdemsie1377@gmail.com>
2026-01-18 12:59:55 -0800

    Add constructor shell with colors and email pattern

--- BNS/src/main/views/LoginPanel.java
+++ BNS/src/main/views/LoginPanel.java
@@ -20,5 +20,25 @@ public class LoginPanel extends JPanel {
     public LoginPanel(Application app) {
         setLayout(new GridLayout(1, 2));
         setBackground(Color.WHITE);
+        // --- LEFT SIDE: BRANDING PANEL ---
+        JPanel brandingPanel = new GradientOverlayPanel();
+        brandingPanel.setLayout(new BoxLayout(brandingPanel, BoxLayout.Y_AXIS));
+        brandingPanel.setBorder(new EmptyBorder(60, 60, 60, 60));
+
+        JLabel version = new JLabel("v2.5.0 • ENTERPRISE SECURE");
+        version.setForeground(new Color(255, 255, 255, 150));
+        version.setFont(new Font("SansSerif", Font.BOLD, 10));
+
+        JLabel heroText = new JLabel("<html><body style='width: 300px'>"
+                + "<h1 style='color:white; font-size:40pt; font-style:italic;'>Efficiency in <br>"
+                + "<font color='#818CF8'>Every Ward.</font></h1><br>"
+                + "<p style='color:#CBD5E1; font-size:14pt;'>Streamlining bed management and patient notifications with real-time intelligence.</p>"
+                + "</body></html>");
+        
+        brandingPanel.add(version);
+        brandingPanel.add(Box.createVerticalGlue());
+        brandingPanel.add(heroText);
+        brandingPanel.add(Box.createVerticalGlue());
+
 }
-}
+
