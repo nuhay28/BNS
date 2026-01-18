da486e897eb96f10e6bd0651cf914584fda22a79
Netsanet Demsie <netsanetdemsie1377@gmail.com>
2026-01-18 12:47:38 -0800

    Add constructor shell with colors and email pattern

--- BNS/src/main/views/LoginPanel.java
+++ BNS/src/main/views/LoginPanel.java
@@ -9,5 +9,16 @@ public class LoginPanel extends JPanel {
     private JTextField emailField;
     private JPasswordField passField;
     private AuthService authService = new AuthService();
-}
 
+    private Color indigo600 = new Color(79, 70, 229);
+    private Color slate900 = new Color(15, 23, 42);
+    private Color slate500 = new Color(100, 116, 139);
+    private Color slate100 = new Color(241, 245, 249);
+
+    private static final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@(.+)$";
+
+    public LoginPanel(Application app) {
+        setLayout(new GridLayout(1, 2));
+        setBackground(Color.WHITE);
+}
+}
